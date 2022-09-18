105effcebe7e8062490b717b6051b6dcb0fe0ef8
Rohit Tank <rowhittank@gmail.com>
2022-09-18 20:08:15 +0530

    About us - header and footer (alignment and carousel)

--- AboutUs/about.php
+++ AboutUs/about.php
@@ -1,15 +1,46 @@
-<!doctype html>
+
+
+
+<?php 
+    include '../TemplateHTML/boilerplate.html';
+?>
+
+<link rel="stylesheet" href="style1.css">
+
+<title>About Us</title> 
+<style>
+  footer {
+    margin-top: 900px;
+  }
+
+  h5{
+    color: rgb(119, 198, 2);
+    font-size: 25px;
+    font-weight: 600;
+  }
+
+
+</style>
+
+<?php
+    include '../TemplateHTML/Navbar/home.html';
+?>
+
+<!-- <!doctype html>
 <html lang="en">
   <head>
-    <!-- Required meta tags -->
+    <!-- Required meta tags --><!--
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@100&family=Roboto:wght@300&display=swap"
+    rel="stylesheet">
     <link rel="stylesheet" href="./css/style.css">
     <title>About Us</title>
   </head>
-  <body>
+  <body> -->
     <div class="aboutus-header">
         <h2 style="color: white;">About Us</h2>
     </div>
@@ -17,7 +48,9 @@
         <div class="top-content">
             <img src="./imgABT/img/about.png" class="img-fluid" alt="">
             <div class="top-info">
-                <h2>Innovative Way To reuse, recyle, reduce</h2><br><br>
+              <center>
+                <h5>Innovative Way To reuse, recyle, reduce</h5><br><br>
+              </center>
                 <p>E-Regain was established in 2022 and is headquartered in Pune, Maharashtra. Our company is a leading player in providing efficient, ethical and environmentally friendly services. It helps a typical person like u and me to manage outdated technology or even technology that doesn't function anymore in all shapes and sizes. And sell to vendors that recycle assets in a safe, secure and responsible manner.</p>
             </div>
 
@@ -26,7 +59,9 @@
             <img src="./imgABT/img/about1.jpg" class="img-fluid1" alt="">
           
             <div class="bottom-info">
-                <h2>Do your part. Recycling is a shared responsibility!</h2><br><br>
+              <center>
+                <h5>Do your part. Recycling is a shared responsibility!</h5><br><br>
+              </center>
                 <p>Our qualified staff handles the logistics, project management and all other aspects of your end-of-life technology retirement. U as a customer receives a decent amount based on what u sell us. We make sure public get the right amount for their appliances whatever the condition maybe. There's an option to donate your e-product as an act of generosity and contribute in welfare of mother earth. We then sell the technologies again to the vendors who are trusted and have great records in the book of history. Then your e-appliances are recycled by these vendors.</p>
             </div>
 
@@ -44,7 +79,7 @@
                     <!-- our vision -->
                     <div class="carousel-item active">
     
-                            <h3 class="m-0">Our Mission</h3>
+                            <h5 class="m-0">Our Mission</h5>
                             <br><br><br>
                             <h4 class="font-weight-normal mb-4">To create a safe, efficient and effective e-waste management, disposal, and recycling system that protects the natural environment while supporting the city</h4>
                     </div> 
@@ -69,23 +104,20 @@
             </button>
           </div>
     </div>
-<div class="links">
-        <a href="../Services/SellDonate.php">Services</a><br>
-        <a href="../ContactUs/main.html">Contact Us</a><br>
-        <a href="../FAQS/index.html">FAQS</a><br>
-        <a href="../Testimonials/snippets.html">Testimonials</a><br>
-        <a href="../LoginRegister/login.html">Login</a><br>
-</div>
   
        
+<?php
+    include '../TemplateHTML/Footer/footer.html';
+?>
    
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-   
-    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 
+
+
+
+
+    
   </body>
-</html>
+</html> 

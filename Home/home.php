ccab84ed4c9951afd0cbb7a6e6c961cc3a4ed396
Rohit Tank <rowhittank@gmail.com>
2022-10-03 00:38:32 +0530

    Footer to be added and company list

---
+++ Home/home.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head> 
+  <meta charset="utf-8">
+  <title>E-Sanchay</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./style.css">
+</head>
+ <body>  
+	
+		<div class="socials">
+			<a href="#" class="fa fa-twitter"></a>
+			<a href="https://www.instagram.com/e_sanchay/" class="fa fa-instagram"></a>
+			<a href="https://www.facebook.com/E_regain-100474962806088/?ref=pages_you_manage" class="fa fa-facebook"></a>
+		</div>
+			
+		   <nav class="header">
+			<a class="navbar-brand" href="./home.php">
+				<div class="imgLogo">
+					<img src="../Images/logo2.png" alt="">	
+				</div>
+				<div class="headerSanch">
+					<h1 style="display: inline; ">E Sanchay</h1>
+				</div>
+			</a>
+			<!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			  <span class="navbar-toggler-icon" ></span> -->
+			<!-- </button>-->
+			
+			 <ul> 
+			   <li><a data-red="true" href="#">Home</a></li>
+			   <li><a data-red="true" href="../AboutUs/about.php">About us</a></li>
+			   <li><a data-red="true" href="../Services/SellDonate.php">Services</a></li>
+			   <!-- <li><a href="#">Testimonials</a></li>
+			   <li><a href="#">FAQs</a></li> -->
+			   <li><a data-red="true" href="../ContactUs/main.php">Contact us</a></li>
+			   <li><a data-red="true" href="../LoginRegister/login.php">Login/Sign up</a></li>
+			 </ul>
+			</nav>
+			<div class="image-wrapper">
+				<img src="../Images/old-electronics.jpg" class="img-fluid" alt="...">
+				<div class="punchline">
+					<figure class="text-center">
+						<blockquote class="blockquote-heading"> <p> E-SANCHAY</p> </blockquote>
+							<blockquote class="blockquote">
+							  <p>TRADE-IN YOUR USED <br>
+								ELECTRONICS for CASH</p>
+							</blockquote>
+						
+						<figcaption class="blockquote-footer">
+							  An innovative way to <cite title="Source Title">reuse, recyle, reduce</cite>
+						</figcaption>
+						<a href="../Services/SellDonate.php">
+							<button class="button-19" role="button">SELL</button>
+						</a>
+					  </figure>
+					  
+				</div>
+				
+				
+			</div>  
+
+			<?php include '../TemplateHTML/Footer/footer.html';?>
+			  
+			<!-- cards start -->
+		
+				<div class="card-group">
+				<div class="card">
+				  <img src="./images/images/Page 17.jpeg" class="card-img-top mx-auto" alt="...">
+				  <div class="card-body">
+					<h5 class="card-title">Fill details</h5>
+					<p class="card-text">Find your used electronics and fill in the details based on the condition.</p>
+				  </div>
+				</div>
+				<div class="card">
+				  <img src="./images/images/Page 16.jpeg" class="card-img-top mx-auto" alt="...">
+				  <div class="card-body">
+					<h5 class="card-title">Pickup Facility</h5>
+					<p class="card-text">We provide you with a free, trackable pre-paid shipping label for sending us your item(s).</p>
+				  </div>
+				</div>
+				<div class="card">
+				  <img src="./images/images/Page 15.jpeg" class="card-img-top mx-auto" alt="...">
+				  <div class="card-body">
+					<h5 class="card-title">Get Cash</h5>
+					<p class="card-text">No need to wait for a buyer, get cash fast through our website</p>
+				  </div>
+				</div>
+			</div>
+
+
+
+
+
+			<!-- company stuff -->
+			<div class="companies">
+				<section>
+					<div class="container">
+						<div class="row">
+							
+							
+							<!--Profile Card 5-->
+							<div class="col-md-4 mt-4">
+								<div class="card profile-card-5">
+									<div class="card-img-block">
+										<center>
+											<img class="card-img-top" src="../Images/Home_Company/comm.png" alt="Card image cap">
+										</center>
+									</div>
+									<div class="card-body pt-0">
+										<center>
+											<p class="card-text">The platform brings together individuals, vendors, distributor  distributors, OEM'S and other customers active in area of reuse of electronic products.</p>
+											<h5 class="card-title">Re commerce</h5>
+										</center>
+									
+								  </div>
+								</div>
+								<!-- <p class="mt-3 w-100 float-left text-center"><strong>Card with Floting Picture</strong></p> -->
+							</div>
+							<!--Profile Card 5-->
+							 <!--Profile Card 5-->
+							 <div class="col-md-4 mt-4">
+								<div class="card profile-card-5">
+									<div class="card-img-block">
+										<center>
+											<img class="card-img-top" src="../Images/Home_Company/better.png" alt="Card image cap">
+										</center>
+									</div>
+									<div class="card-body pt-0">
+										<center>
+											<p class="card-text">Our Better World is the digital storytelling initiative of the Singapore International Foundation, which brings world communities together to do good.</p>
+											<h5 class="card-title">Our Better World </h5>
+										</center>
+									
+								  </div>
+								</div>
+								<!-- <p class="mt-3 w-100 float-left text-center"><strong>Card with Floting Picture</strong></p> -->
+							</div>
+							<!--Profile Card 5-->
+							 <!--Profile Card 5-->
+							 <div class="col-md-4 mt-4">
+								<div class="card profile-card-5">
+									<div class="card-img-block">
+										<center>
+											<img class="card-img-top" src="../Images/Home_Company/lg.png" alt="Card image cap">
+										</center>
+									</div>
+									<div class="card-body pt-0">
+										<center>
+											<p class="card-text">LG Electronics India provides an e-waste take-back service for old LG Products in compliance with the EPR (Extended Producer Responsibility) LG seeks to contribute to conserving natural resources and protecting the environment</p>
+											<h5 class="card-title">Life's Good</h5>
+										</center>
+										
+									
+								  </div>
+								</div>
+								<!-- <p class="mt-3 w-100 float-left text-center"><strong>Card with Floting Picture</strong></p> -->
+							</div>
+							<!--Profile Card 5-->   
+							
+						</div>
+						<div class="row">
+							
+							
+							<!--Profile Card 5-->
+							<div class="col-md-4 mt-4">
+								<div class="card profile-card-5">
+									<div class="card-img-block">
+										<center>
+											<img class="card-img-top" src="../Images/Home_Company/tripple.png" alt="Card image cap">
+										</center>
+									</div>
+									<div class="card-body pt-0">
+										<center>
+											<p class="card-text">The company provides services for recycling of ferrous and non-ferrous scrap metals. It is among leading suppliers of services pertaining to transforming scrap into metal stampings, turnings, plates, extrusions, sheets, wires, bars, pipes, tubes</p>
+											<h5 class="card-title">Tripple M</h5>
+										</center>
+									
+								  </div>
+								</div>
+								<!-- <p class="mt-3 w-100 float-left text-center"><strong>Card with Floting Picture</strong></p> -->
+							</div>
+							<!--Profile Card 5-->
+							 <!--Profile Card 5-->
+							 <div class="col-md-4 mt-4">
+								<div class="card profile-card-5">
+									<div class="card-img-block">
+										<center>
+											<img class="card-img-top" src="../Images/Home_Company/greenscape.png" alt="Card image cap">
+										</center>
+									</div>
+									<div class="card-body pt-0">
+										<center>
+											<p class="card-text">Greenspace Eco Management has been incepted in late 2007 and has grown to be India's leading provider of IT Asset Disposition services.</p>
+											<h5 class="card-title">Greenspace Eco Management</h5>
+										</center>
+									
+								  </div>
+								</div>
+								<!-- <p class="mt-3 w-100 float-left text-center"><strong>Card with Floting Picture</strong></p> -->
+							</div>
+							<!--Profile Card 5-->
+							 <!--Profile Card 5-->
+							 <div class="col-md-4 mt-4">
+								<div class="card profile-card-5">
+									<div class="card-img-block">
+										<center>
+											<img class="card-img-top" src="../Images/Home_Company/canara.png" alt="Card image cap">
+										</center>
+									</div>
+									<div class="card-body pt-0">
+										<center>
+											<p class="card-text">The bank is forerunner in implementation of IT related products services and continuously making efforts to provide sustainable solution for e waste</p>
+											<h5 class="card-title">Canara Bank</h5>
+										</center>
+									
+								  </div>
+								</div>
+								<!-- <p class="mt-3 w-100 float-left text-center"><strong>Card with Floting Picture</strong></p> -->
+							</div>
+							<!--Profile Card 5-->   
+							
+						</div>
+					</div>
+				</section>
+			</div>
+		
+
+
+			<!-- footer -->
+
+		<!-- cards end -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+ </body>
+
+ </html>

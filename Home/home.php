972d4ffee9f42c03122b016a4db30d137f126fef
Rohit Tank <rowhittank@gmail.com>
2022-10-03 21:01:30 +0530

    image

--- Home/home.php
+++ Home/home.php
@@ -7,7 +7,14 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="./style.css">
+  <link rel="stylesheet" href="./style1.css">
+  <style>
+	.site-footer {
+		background-color: #26272b;
+
+	}
+	
+  </style>
 </head>
  <body>  
 	
@@ -54,7 +61,7 @@
 							  An innovative way to <cite title="Source Title">reuse, recyle, reduce</cite>
 						</figcaption>
 						<a href="../Services/SellDonate.php">
-							<button class="button-19" role="button">SELL</button>
+							<button class="button-19" role="button">SELL/DONATE</button>
 						</a>
 					  </figure>
 					  
@@ -63,10 +70,15 @@
 				
 			</div>  
 
-			<?php include '../TemplateHTML/Footer/footer.html';?>
 			  
+			
+			<section>
+				<center>
+					<h1 style="color: #3b945e; font-weight:bold; margin-top:30px; text-transform:uppercase;">Get cash in three easy steps</h1>
+				</center>
+			</section>
 			<!-- cards start -->
-		
+			
 				<div class="card-group">
 				<div class="card">
 				  <img src="./images/images/Page 17.jpeg" class="card-img-top mx-auto" alt="...">
@@ -92,145 +104,26 @@
 			</div>
 
 
+			<section>
+        		<div class="companyimg">
+					<img src="companyDet.png" alt="" srcset="" style="width:100%; height: 900px;">
+        		</div>
+    		</section>
+
+
+
+			<!-- HTML !-->
+
+
 
 
 
 			<!-- company stuff -->
-			<div class="companies">
-				<section>
-					<div class="container">
-						<div class="row">
-							
-							
-							<!--Profile Card 5-->
-							<div class="col-md-4 mt-4">
-								<div class="card profile-card-5">
-									<div class="card-img-block">
-										<center>
-											<img class="card-img-top" src="../Images/Home_Company/comm.png" alt="Card image cap">
-										</center>
-									</div>
-									<div class="card-body pt-0">
-										<center>
-											<p class="card-text">The platform brings together individuals, vendors, distributor  distributors, OEM'S and other customers active in area of reuse of electronic products.</p>
-											<h5 class="card-title">Re commerce</h5>
-										</center>
-									
-								  </div>
-								</div>
-								<!-- <p class="mt-3 w-100 float-left text-center"><strong>Card with Floting Picture</strong></p> -->
-							</div>
-							<!--Profile Card 5-->
-							 <!--Profile Card 5-->
-							 <div class="col-md-4 mt-4">
-								<div class="card profile-card-5">
-									<div class="card-img-block">
-										<center>
-											<img class="card-img-top" src="../Images/Home_Company/better.png" alt="Card image cap">
-										</center>
-									</div>
-									<div class="card-body pt-0">
-										<center>
-											<p class="card-text">Our Better World is the digital storytelling initiative of the Singapore International Foundation, which brings world communities together to do good.</p>
-											<h5 class="card-title">Our Better World </h5>
-										</center>
-									
-								  </div>
-								</div>
-								<!-- <p class="mt-3 w-100 float-left text-center"><strong>Card with Floting Picture</strong></p> -->
-							</div>
-							<!--Profile Card 5-->
-							 <!--Profile Card 5-->
-							 <div class="col-md-4 mt-4">
-								<div class="card profile-card-5">
-									<div class="card-img-block">
-										<center>
-											<img class="card-img-top" src="../Images/Home_Company/lg.png" alt="Card image cap">
-										</center>
-									</div>
-									<div class="card-body pt-0">
-										<center>
-											<p class="card-text">LG Electronics India provides an e-waste take-back service for old LG Products in compliance with the EPR (Extended Producer Responsibility) LG seeks to contribute to conserving natural resources and protecting the environment</p>
-											<h5 class="card-title">Life's Good</h5>
-										</center>
-										
-									
-								  </div>
-								</div>
-								<!-- <p class="mt-3 w-100 float-left text-center"><strong>Card with Floting Picture</strong></p> -->
-							</div>
-							<!--Profile Card 5-->   
-							
-						</div>
-						<div class="row">
-							
-							
-							<!--Profile Card 5-->
-							<div class="col-md-4 mt-4">
-								<div class="card profile-card-5">
-									<div class="card-img-block">
-										<center>
-											<img class="card-img-top" src="../Images/Home_Company/tripple.png" alt="Card image cap">
-										</center>
-									</div>
-									<div class="card-body pt-0">
-										<center>
-											<p class="card-text">The company provides services for recycling of ferrous and non-ferrous scrap metals. It is among leading suppliers of services pertaining to transforming scrap into metal stampings, turnings, plates, extrusions, sheets, wires, bars, pipes, tubes</p>
-											<h5 class="card-title">Tripple M</h5>
-										</center>
-									
-								  </div>
-								</div>
-								<!-- <p class="mt-3 w-100 float-left text-center"><strong>Card with Floting Picture</strong></p> -->
-							</div>
-							<!--Profile Card 5-->
-							 <!--Profile Card 5-->
-							 <div class="col-md-4 mt-4">
-								<div class="card profile-card-5">
-									<div class="card-img-block">
-										<center>
-											<img class="card-img-top" src="../Images/Home_Company/greenscape.png" alt="Card image cap">
-										</center>
-									</div>
-									<div class="card-body pt-0">
-										<center>
-											<p class="card-text">Greenspace Eco Management has been incepted in late 2007 and has grown to be India's leading provider of IT Asset Disposition services.</p>
-											<h5 class="card-title">Greenspace Eco Management</h5>
-										</center>
-									
-								  </div>
-								</div>
-								<!-- <p class="mt-3 w-100 float-left text-center"><strong>Card with Floting Picture</strong></p> -->
-							</div>
-							<!--Profile Card 5-->
-							 <!--Profile Card 5-->
-							 <div class="col-md-4 mt-4">
-								<div class="card profile-card-5">
-									<div class="card-img-block">
-										<center>
-											<img class="card-img-top" src="../Images/Home_Company/canara.png" alt="Card image cap">
-										</center>
-									</div>
-									<div class="card-body pt-0">
-										<center>
-											<p class="card-text">The bank is forerunner in implementation of IT related products services and continuously making efforts to provide sustainable solution for e waste</p>
-											<h5 class="card-title">Canara Bank</h5>
-										</center>
-									
-								  </div>
-								</div>
-								<!-- <p class="mt-3 w-100 float-left text-center"><strong>Card with Floting Picture</strong></p> -->
-							</div>
-							<!--Profile Card 5-->   
-							
-						</div>
-					</div>
-				</section>
-			</div>
-		
+			
 
 
 			<!-- footer -->
+			<?php include '../TemplateHTML/Footer/footer.html';?>
 
 		<!-- cards end -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>

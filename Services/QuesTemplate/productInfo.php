fee7ce45d1fd9efe1e2283dc267a2114202f4090
Rohit Tank <rowhittank@gmail.com>
2022-10-13 00:12:52 +0530

    functionality implemented on fridge => user can sell or donate their fridge and view the status of their request

---
+++ Services/QuesTemplate/productInfo.php
@@ -0,0 +1,18 @@
+<?php
+    include '../TemplateHTML/boilerplate.html';
+?>
+    <title>Product Description</title>
+
+<?php
+    include '../TemplateHTML/Navbar/home.php';   
+?>
+
+
+
+<?php
+
+    include '../TemplateHTML/Footer/footer.html';
+
+
+
+?>
--- Services/inputDB.php
+++ Services/inputDB.php
@@ -3,12 +3,7 @@
  <!-- <input type="hidden" name="varname" value="var_value"> -->
 
 <?php
-
-function noProblem($nameValue){
-    if(!isset($nameValue)) return null;
-    else return $nameValue;
-}
-        
+   session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -17,8 +12,130 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     switch($itemName){
         case "fridge":
-            echo "<h1>$itemName</h1>"; 
-            echo "<h1>$brandName</h1>";
+
+            $condition = $_REQUEST['condition'];
+            $storage = $_REQUEST['storage'];
+            $door = $_REQUEST['door'];
+
+
+            include './QuesTemplate/functions.php';
+            
+            $filename = $_FILES["uploadfile"]["name"];
+
+            uploadImage('Fridge'); ?>
+
+
+
+
+            <?php include '../TemplateHTML/boilerplate.html'; ?>
+            <title>Product Description</title>
+        <?php
+            include '../TemplateHTML/Navbar/home.php';   ?>
+
+
+            <div class="main_container" style="display:flex;">
+                <div class="container text-center" style="width:80%; height:500px; background-color: red;">
+                    <div class="row">
+                        <div class="col-5" style="background-color: yellow;">
+                        <!-- carousel -->
+                        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" style="margin-top:20px; margin-bottom:20px;">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img style="border-radius: 20px;" src="./ProductImage/Fridge/<?php echo $filename; ?>" class="d-block w-100" alt="..." style="height: 600px;">
+                                </div>
+                                <div class="carousel-item">
+                                  <img src="..." class="d-block w-100" alt="...">
+                                </div>
+                                <div class="carousel-item">
+                                  <img src="..." class="d-block w-100" alt="...">
+                                </div>
+                              </div>
+                              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                              </button>
+                              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                              </button>
+                            </div>
+                        </div>
+                        <div class="col-7" style="background-color: green; display:flex; justify-content:center">
+                            
+                            <!-- displaying details of what user has selected -->
+                            <div class="prodDetails" style="height: 500px; width: 400px; background-color: aqua; margin-top: 50px;">
+                                
+                                <h2 class="card-title" style="margin-bottom: 20px;">What u have selected</h2>
+                                        <h5 class="card-text"><?php echo "Brand of fridge: ".$brandName; ?></h5>
+                                        <h5 class="card-text"><?php echo "Condition of fridge: ".$condition; ?></h5>
+                                        <h5 class="card-text"><?php echo "Storage of fridge: ".$storage; ?></h5>
+                                        <h5 class="card-text"><?php echo "No. of doors of fridge: ".$door; ?></h5>
+                                        <a href="./SellItem.php?itemName=fridge" style="margin-top: 15px;" class="btn btn-primary">Edit details</a>
+                                        <?php
+                                            if(isset($_SESSION['logged'])){ 
+                                                // $arr = [$brandName,"#" ,$condition, "#", $storage, $door, "./ProductImage/Fridge/$filename"];
+                                                ?>
+                                                
+                                                <form action="newFile.php" method="POST">
+
+                                                    <input type="hidden" name="arr" value='<?php echo "$brandName#$condition#$storage#$door#"?>'>
+                                                   
+                                                    <input type="hidden" name="fileupload" value="./ProductImage/Fridge/<?php echo $filename; ?>">
+
+                                                    <input type="hidden" name="productCategory" value="fridge">
+
+
+                                                    <!-- <button type="submit" class="btn btn-primary">Go ahead?</button> -->
+                                                    <div class="form-floating" style="margin-left: 10px; margin-right: 10px;">
+                                                      <textarea  class="form-control" placeholder="your address" name="address" id="floatingTextarea"></textarea>
+                                                      <label for="floatingTextarea">Pls enter your address:</label>
+                                                    </div>
+                                                    <button type="submit" name="sell" class="btn btn-primary">Sell</button>
+                                                    <button type="submit" name="donate" class="btn btn-primary">Donate</button>
+
+                                                    <!-- <button type="submit" value="sell" class="btn btn-primary">Sell</button>
+                                                    <button type="submit" value="donate" class="btn btn-primary">Donate</button> -->
+
+                                                </form>
+                                                <!-- <a href="newfile.php" class="btn btn-primary">Go ahead?</a> -->
+
+                                        <?php } else {?>
+                                                 <a href="../LoginRegister/login.php" class="btn btn-primary">Pls login to go ahead</a>
+
+                                        <?php } ?>  
+
+                                </div>
+
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <!-- <center> -->
+                <?php 
+                // echo "<h1>$itemName</h1>"; 
+                //  echo "<h1>$brandName</h1>"; 
+                //  echo "<h1>$condition</h1>"; 
+                //  echo "<h1>$storage</h1>"; 
+                //  echo "<h1>$door</h1>"; 
+                 ?>
+
+                <!-- <img style="width: 300px; height: 300px;" src="./ProductImage/Fridge/<?php echo $filename; ?>" > -->
+
+                <!-- <button class="btn btn-primary">
+                  <a href="./SellItem.php?itemName=fridge" style="color: white; text-decoration:none;">
+                    Go Back
+                  </a>
+                </button>
+
+            </center> -->
+
+
+
+            <?php include '../TemplateHTML/Footer/footer.html';
             break;
         
         case "aircon":
